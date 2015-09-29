7a79b505b920a7096a299f86153bbf247e7e04cd
Thomas Chen <foxnewsnetwork@gmail.com>
2015-09-29 16:55:31 -0700

    added doc title addon for testing purposes

--- blueprints/simwms-build/index.js
+++ blueprints/simwms-build/index.js
@@ -5,7 +5,9 @@ module.exports = {
 
   afterInstall: function() {
     return RSVP.all([
-      this.addPackageToProject('ember-cli-selenium', '~0.1.1')
+      this.addPackageToProject('ember-cli-selenium', '~0.1.1'),
+      this.addPackageToProject('ember-perf', '~0.0.12'),
+      this.addPackageToProject("ember-cli-document-title", "~0.2.0")
     ]);
   }
 }
---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,8 @@
+export default Ember.Route.extend({
+  title: function(tokens) {
+   var base = 'Dummy ~ok';
+   var hasTokens = tokens && tokens.length;
+
+   return hasTokens ? tokens.reverse().join(' - ') + ' - ' + base : base;
+  }
+});
